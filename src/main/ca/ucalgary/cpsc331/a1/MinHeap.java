de71a691edc91ecf9fb080fe72b8eaa0fa2b353e
soswedan <101846470+soswedan@users.noreply.github.com>
2023-10-06 02:25:48 -0600

    no sus changes

--- src/main/ca/ucalgary/cpsc331/a1/MinHeap.java
+++ src/main/ca/ucalgary/cpsc331/a1/MinHeap.java
@@ -25,7 +25,7 @@ public class MinHeap implements PriorityQueue {
         }
         heap[size] = key;
         size++;
-        heapifyUp(size - 1);
+
     }
 
     public int extractMin() {
@@ -34,9 +34,8 @@ public class MinHeap implements PriorityQueue {
         }
         int min = heap[0];
         heap[0] = heap[size - 1];
-        size--;
-        heapifyDown(0);
         return min;
+
     }
 
     public int min() {
@@ -46,33 +45,4 @@ public class MinHeap implements PriorityQueue {
         return heap[0];
     }
 
-    private void heapifyUp(int index) {
-        int parent = (index - 1) / 2;
-        if (index > 0 && heap[index] < heap[parent]) {
-            swap(index, parent);
-            heapifyUp(parent);
-        }
-    }
-
-    private void heapifyDown(int index) {
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-        int smallest = index;
-        if (left < size && heap[left] < heap[smallest]) {
-            smallest = left;
-        }
-        if (right < size && heap[right] < heap[smallest]) {
-            smallest = right;
-        }
-        if (smallest != index) {
-            swap(index, smallest);
-            heapifyDown(smallest);
-        }
-    }
-
-    private void swap(int i, int j) {
-        int temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-    }
 }
